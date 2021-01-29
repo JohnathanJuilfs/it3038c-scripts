3145f1c12b3658f24d8ea7e73bd1bc14c037d2ea
Johnny Juilfs <juilfsjc@mail.uc.edu>
2021-01-29 14:08:03 -0500

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,15 @@
+﻿
+#$Hello = "Hello, PowerShell!"
+#write-Host($Hello)
+function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+
+write-host(getIP)
+$IP = getIP
+$Date = ""
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $. PowerShell version . Today's date is $Date"
+
+write-host($Body)
+
+#Send-MailMessage -To "juilfsjc@mail.uc.edu" -From "juilfsjc@mail.uc.edu" -Subject "IT3038C Windows SysINfO" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
